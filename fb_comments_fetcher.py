1e55bbce409e095394e15334a4ce9b51bee1b4ac
Andrea Grandi <a.grandi@gmail.com>
2017-11-18 15:23:27 +0000

    Initial version of Facebook comments fetcher

---
+++ fb_comments_fetcher.py
@@ -0,0 +1,30 @@
+import facebook
+import os
+
+
+FB_ACCESS_TOKEN = os.environ.get('FB_ACCESS_TOKEN')
+MAX_NUMBER_OF_COMMENTS = 100
+
+
+graph = facebook.GraphAPI(FB_ACCESS_TOKEN)
+profile = graph.get_object(id='me')
+posts = graph.get_all_connections(profile['id'], 'posts')
+
+posts_count = 0
+comments_count = 0
+
+with open('comments.tsv', 'w') as f:
+    for post in posts:
+        posts_count += 1
+        print('Post: {0}'.format(posts_count))
+        comments = graph.get_all_connections(id=post['id'], connection_name='comments')
+
+        for comment in comments:
+            comments_count += 1
+            print('Comment: {0} - {1}'.format(comments_count, comment['message']))
+            f.write(comment['id'] + "\t" + comment['message'] + "\n")
+            if comments_count >= MAX_NUMBER_OF_COMMENTS:   # Stop after 100 comments
+                break
+
+        if comments_count >= MAX_NUMBER_OF_COMMENTS:   # Stop after 100 comments
+                break
